07d62b5cea0003e64e8f67438336fe34a9eb8b47
c8e4 <c8e4@proton.me>
2024-05-09 23:43:43 +0200

    refactor buy contract

--- src/lib/server/turbo-mempool/buy-contract/contract-buy-token-for-erg.ts
+++ src/lib/server/turbo-mempool/buy-contract/contract-buy-token-for-erg.ts
@@ -9,96 +9,91 @@ export const buyTokenForErg = `{
 	def getBuyRate(box: Box)               = box.R7[Long].get
 	def getBuyerMultisigAddress(box: Box)  = box.R8[Coll[Byte]].get
 
-	def getTokenAmount(box: Box) = box.tokens(0)._2
-  
+	def tokenId(box: Box) = 
+	  box.tokens(0)._1
+	def tokenAmount(box: Box) = 
+	  box.tokens(0)._2
+
 	def isSameContract(box: Box) = 
 		  box.propositionBytes == SELF.propositionBytes
-  
+
   def isSameTokenId (box: Box) = 
-	  	getTokenId(SELF) == getTokenId(box)
+		  getTokenId(SELF) == getTokenId(box)
 
-	def includesToken(box: Box)    = 
-	  	getTokenId(SELF) == getTokenId(box) &&
-	  	box.tokens.size > 0 &&
-		  getTokenId(SELF) == box.tokens(0)._1 
-
+	def includesToken(box: Box) = 
+		  getTokenId(SELF) == getTokenId(box) &&
+		  box.tokens.size > 0 &&
+		  getTokenId(SELF) == tokenId(box)
 
   def isRateGreaterZero(box:Box) =
-      getBuyRate(box)>0
+	  getBuyRate(box) > 0
 
 	def isSameBuyer(box: Box)   = 
-      getBuyerPk(SELF) == getBuyerPk(box) &&
-      getPoolPk(SELF) == getPoolPk(box)
+	  getBuyerPk(SELF) == getBuyerPk(box) &&
+	  getPoolPk(SELF) == getPoolPk(box)
 
   def isSameUnlockHeight(box: Box)  = 
-      unlockHeight(SELF) == unlockHeight(box)
+	  unlockHeight(SELF) == unlockHeight(box)
 
   def isSameMultisig(box: Box)    =
-      getBuyerMultisigAddress(SELF) == getBuyerMultisigAddress(box)
+	  getBuyerMultisigAddress(SELF) == getBuyerMultisigAddress(box)
 
-  	def isLegitBuyOrderInput(box: Box) = {
-    isSameBuyer(box) &&
-    isSameUnlockHeight(box) && 
-    isSameTokenId(box) &&
-    isRateGreaterZero(box) &&
-    isSameMultisig(box) &&
-		isSameContract(box)
-    }
+	def isLegitBuyOrderInput(box: Box) =
+	  isSameBuyer(box) &&
+	  isSameUnlockHeight(box) && 
+	  isSameTokenId(box) &&
+	  isRateGreaterZero(box) &&
+	  isSameMultisig(box) &&
+	  isSameContract(box)
 
-  	val minBuyRate = INPUTS.filter(isLegitBuyOrderInput).fold(0L, {(r:Long, box:Box) => {
-	  if(r < getBuyRate(box)) r else getBuyRate(box)
-	  }})
+	val minBuyRate = INPUTS
+	  .filter(isLegitBuyOrderInput)
+	  .fold(0L, {(r:Long, box:Box) => {
+			if(r < getBuyRate(box)) r else getBuyRate(box)
+		}})
 
-  	def isLegitBuyOrderOutput(box: Box) = {
-    isLegitBuyOrderInput(box)&&
-    minBuyRate == getBuyRate(box)
-    }
-  
-	def isPaymentBox(box:Box) = {
-		isSameBuyer(box) &&
-    isSameUnlockHeight(box) &&
-		includesToken(box) &&
-		getBuyerMultisigAddress(SELF) == box.propositionBytes
-	}
-  
-  def sumValuesIn(boxes: Coll[Box]): Long = 
-		boxes
+  def isLegitBuyOrderOutput(box: Box) =
+	  isLegitBuyOrderInput(box)&&
+	  minBuyRate == getBuyRate(box)
+
+	def isPaymentBox(box:Box) =
+		  isSameBuyer(box) &&
+	  isSameUnlockHeight(box) &&
+		  includesToken(box) &&
+		  getBuyerMultisigAddress(SELF) == box.propositionBytes
+
+  def sumValuesIn(boxes: Coll[Box]): Long = boxes
 			.filter(isLegitBuyOrderInput) 
 			.fold(0L, {(a:Long, b: Box) => a + b.value})
 
-  def sumValuesOut(boxes: Coll[Box]): Long = 
-  boxes
-    .filter(isLegitBuyOrderOutput) 
-    .fold(0L, {(a:Long, b: Box) => a + b.value})
+  def sumValuesOut(boxes: Coll[Box]): Long = boxes
+	  .filter(isLegitBuyOrderOutput) 
+	  .fold(0L, {(a:Long, b: Box) => a + b.value})
 
-  def sumAmountsIn(boxes: Coll[Box]): Long = 
-		boxes
+  def sumAmountsIn(boxes: Coll[Box]): Long = boxes
 			.filter(isLegitBuyOrderInput) 
 			.fold(0L, {(a:Long, b: Box) => a + b.value/getBuyRate(b)})
-  
-  def sumAmountsOut(boxes: Coll[Box]): Long = 
-  boxes
-    .filter(isLegitBuyOrderOutput) 
-    .fold(0L, {(a:Long, b: Box) => a + b.value/getBuyRate(b)})
-  
+
+  def sumAmountsOut(boxes: Coll[Box]): Long = boxes
+	  .filter(isLegitBuyOrderOutput) 
+	  .fold(0L, {(a:Long, b: Box) => a + b.value/getBuyRate(b)})
+
   val valuesIn: Long  = sumValuesIn(INPUTS)
   val amountsIn: Long = sumAmountsIn(INPUTS)
 
   val valuesOut: Long = sumValuesOut(OUTPUTS) 
   val amountsOut: Long = sumAmountsOut(OUTPUTS) 
 
-  val deltaAmounts = amountsIn-amountsOut
-  val deltaValues = valuesIn-valuesOut
+  val deltaAmounts = amountsIn - amountsOut
+  val deltaValues = valuesIn - valuesOut
 
-  def tokensBought(boxes: Coll[Box]): Long = 
-  boxes
-    .filter(isPaymentBox) 
-    .fold(0L, {(a:Long, b: Box) => a + b.tokens(0)._2})
+  def tokensBought(boxes: Coll[Box]): Long = boxes
+	  .filter(isPaymentBox) 
+	  .fold(0L, {(a:Long, b: Box) => a + tokenAmount(b)})
 
-  val sendToBuyer = tokensBought(OUTPUTS)
+  val sentToBuyer = tokensBought(OUTPUTS)
+  val isBuyerPaid = deltaAmounts <= sentToBuyer 
 
-  val isBuyerPaid = deltaAmounts <= sendToBuyer 
-  
 	if(HEIGHT > unlockHeight(SELF)){
 		getBuyerPk(SELF)
 	}else{
