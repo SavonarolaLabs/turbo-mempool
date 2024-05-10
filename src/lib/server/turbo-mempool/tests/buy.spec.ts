7ee71d6a4180e4bd4dc37243843a6f9a4ca510c9
c8e4 <c8e4@proton.me>
2024-05-11 01:21:29 +0200

    cleanup

--- src/lib/server/turbo-mempool/tests/buy.spec.ts
+++ src/lib/server/turbo-mempool/tests/buy.spec.ts
@@ -1,17 +1,11 @@
 import { beforeAll, describe, expect, it } from 'vitest';
-import { sellOrderAddress } from '$lib/server/constants/sellOrder';
 import {
-	ErgoAddress,
-	OutputBuilder,
-	RECOMMENDED_MIN_FEE_VALUE,
-	SAFE_MIN_BOX_VALUE,
-	TransactionBuilder,
-	type Box
+	ErgoAddress, SAFE_MIN_BOX_VALUE, type Box
 } from '@fleet-sdk/core';
-import { ALICE_ADDRESS, BOB_ADDRESS, BUY_ORDER_ADDRESS, DEPOSIT_ADDRESS } from '$lib/server/constants/addresses';
+import { BOB_ADDRESS, BUY_ORDER_ADDRESS, DEPOSIT_ADDRESS } from '$lib/server/constants/addresses';
 import { PRINTER_ADDRESS, PRINTER_MNEMONIC, PRINTER_UTXO } from '../mock/utxos';
 import { deposit } from '../utils/account';
-import { signMultisig, signTx, signTxAllInputs, signTxMulti } from '$lib/server/multisig/multisig';
+import { signTx, signTxMulti } from '$lib/server/multisig/multisig';
 import { buy } from '../utils/buy';
 import { BOB_MNEMONIC } from '$lib/server/constants/mnemonics';
 
@@ -28,7 +22,6 @@ const BUY_ORDER_TOKEN = {
 };
 const RATE = 1n;
 
-//let depositBox;
 let buyOrderBox: Box;
 
 describe('buy', () => {
@@ -42,7 +35,6 @@ describe('buy', () => {
 			BUY_ORDER_TOKEN,
 			10n * SAFE_MIN_BOX_VALUE
 		);
-
 		const depositTx = await signTx(depositUTx, PRINTER_MNEMONIC);
 		const depositBox = depositTx.outputs.find(
 			(o) => o.ergoTree == ErgoAddress.fromBase58(DEPOSIT_ADDRESS).ergoTree
@@ -58,7 +50,6 @@ describe('buy', () => {
 			SAFE_MIN_BOX_VALUE
 		)
 		const buyOrderTx = await signTxMulti(buyOrderUTx, BUYER_MNEMONIC, BUYER_PK);
-
 		buyOrderBox = (buyOrderTx.outputs.find(
 			(o) => o.ergoTree == ErgoAddress.fromBase58(BUY_ORDER_ADDRESS).ergoTree
 		) as Box)
