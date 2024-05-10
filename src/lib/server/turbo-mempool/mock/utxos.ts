5c44787a80724dc4f03a1dca6e1d7f698355bfb6
c8e4 <c8e4@proton.me>
2024-05-10 20:14:34 +0200

    define funding utxo

--- src/lib/server/turbo-mempool/mock/utxos.ts
+++ src/lib/server/turbo-mempool/mock/utxos.ts
@@ -1,3 +1,6 @@
+import { ALICE_ADDRESS } from "$lib/server/constants/addresses";
+import { ALICE_MNEMONIC } from "$lib/server/constants/mnemonics";
+
 export const bobUtxosInitial = [
 	{
 		boxId: 'd2d0deb3b0b2c511e523fd43ae838ba7b89e4583f165169b90215ff11d942c1f',
@@ -64,3 +67,83 @@ export const aliceUtxosInitial = [
 		comment: ''
 	}
 ];
+
+export const fundingUtxo = [
+    {
+      boxId: "200f8b94cc1a8b726bb40450d3ee684bb4506cdf3867000d0d5a67bd15af930e",
+      transactionId: "325e71adc66f484a44cb445a443d6a310bc490a65d6a5179c6dff9ac9ac1cb2d",
+      blockId: "b99a54b2039ecb6e1a0a2c884e12ab028191419e0dbdd63dcd295128f61ddb6d",
+      value: "994600000",
+      index: 2,
+      globalIndex: 5491374,
+      creationHeight: 1261482,
+      settlementHeight: 1261484,
+      ergoTree: "0008cd02eb083423041003740c9e791b2fea5ecf6e273669630a25b7ecabf9145395e447",
+      ergoTreeConstants: "",
+      ergoTreeScript: "{SigmaProp(ProveDlog(ECPoint(eb0834,848edd,...)))}",
+      address: "9gJa6Mict6TVu9yipUX5aRUW87Yv8J62bbPEtkTje28sh5i3Lz8",
+      assets: [],
+      additionalRegisters: {},
+      spentTransactionId: null,
+      mainChain: true
+    }, {
+      boxId: "95f4fe09aeb14e46a7257abc6b60988631bb5dc1d66ae35865c99488420af3bd",
+      transactionId: "325e71adc66f484a44cb445a443d6a310bc490a65d6a5179c6dff9ac9ac1cb2d",
+      blockId: "b99a54b2039ecb6e1a0a2c884e12ab028191419e0dbdd63dcd295128f61ddb6d",
+      value: "1000000",
+      index: 0,
+      globalIndex: 5491372,
+      creationHeight: 1261482,
+      settlementHeight: 1261484,
+      ergoTree: "0008cd02eb083423041003740c9e791b2fea5ecf6e273669630a25b7ecabf9145395e447",
+      ergoTreeConstants: "",
+      ergoTreeScript: "{SigmaProp(ProveDlog(ECPoint(eb0834,848edd,...)))}",
+      address: "9gJa6Mict6TVu9yipUX5aRUW87Yv8J62bbPEtkTje28sh5i3Lz8",
+      assets: [
+        {
+          tokenId: "b73a806dee528632b8d76f07813a1f1b66b8e11bc32b3ad09f8051265f3664ab",
+          index: 0,
+          amount: "50000000000000000",
+          name: "TestToken Test2",
+          decimals: 9,
+          type: "EIP-004"
+        }
+      ],
+      additionalRegisters: {
+        R4: "0e0f54657374546f6b656e205465737432",
+        R5: "0e00",
+        R6: "0e0139"
+      },
+      spentTransactionId: null,
+      mainChain: true
+    }, {
+      boxId: "e7f7fd708e42aca91c47cd3717dc4cb92b343971a9873592c8ecde768bd05f0c",
+      transactionId: "cbe878ba916b5462e48b040f6f268ef4ed6e47722a901b5afdc5dd2873a98b1c",
+      blockId: "ccf62fa8022532d2f1d2ed80a7e0b7527aae177479d07fbdd72486e8b479bff6",
+      value: "1000000",
+      index: 0,
+      globalIndex: 5490189,
+      creationHeight: 1261443,
+      settlementHeight: 1261445,
+      ergoTree: "0008cd02eb083423041003740c9e791b2fea5ecf6e273669630a25b7ecabf9145395e447",
+      ergoTreeConstants: "",
+      ergoTreeScript: "{SigmaProp(ProveDlog(ECPoint(eb0834,848edd,...)))}",
+      address: "9gJa6Mict6TVu9yipUX5aRUW87Yv8J62bbPEtkTje28sh5i3Lz8",
+      assets: [
+        {
+          tokenId: "d2d0deb3b0b2c511e523fd43ae838ba7b89e4583f165169b90215ff11d942c1f",
+          index: 0,
+          amount: "1000000000000000",
+          name: "SwapToken Test1",
+          decimals: 9,
+          type: "EIP-004"
+        }
+      ],
+      additionalRegisters: {},
+      spentTransactionId: null,
+      mainChain: true
+    }
+]
+
+export const FUNDING_MNEMONIC = ALICE_MNEMONIC
+export const FUNDING_ADDRESS = ALICE_ADDRESS
